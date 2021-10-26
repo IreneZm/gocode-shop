0af35081f635d76214078900ca443434f9211fc3
Irene <irene.smolchenko@zoominsoftware.com>
2021-10-26 11:15:05 +0300

    use loop and props to render products

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,18 +1,34 @@
+import { Component } from "react";
 import Product from "../Product/Product";
 import "./Products.css";
-import { Component } from "react";
 
 class Products extends Component {
   render() {
+    const { data } = this.props;
+
     return (
       <section className="products">
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
+        {data.map((product, id) => (
+          <Product key={id} product={product} />
+        ))}
       </section>
+
+      // <section className="products">
+      //   {data.map((product) => (
+      //     <Product
+      //       key={product.id}
+      //       img={product.image}
+      //       price={product.price}
+      //       title={product.title}
+      //     />
+      //   ))}
+      // </section>
+
+      // <section className="products">
+      //   {data.map(({ id, image, price, title }) => (
+      //     <Product key={id} img={image} price={price} title={title} />
+      //   ))}
+      // </section>
     );
   }
 }
