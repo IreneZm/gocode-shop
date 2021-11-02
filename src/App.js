4e665406b61a1b6446b3db9a5cd106c71fb5efa4
Irene <irene.smolchenko@zoominsoftware.com>
2021-11-02 14:47:50 +0200

    fetch with try and catch and loading

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 import Header from "./components/Header/Header";
 import Products from "./components/Products/Products";
+import { CircularProgress } from "@mui/material";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+
 // import { data } from "./data/data";
 import "./App.css";
 
@@ -10,25 +14,45 @@ class App extends Component {
     category: "",
     data: [],
     categories: [],
+    loading: false,
+    error: false,
   };
 
   filterBySelected = (selectedCategory) => {
     this.setState({ category: selectedCategory });
   };
 
+  fetchProducts = async () => {
+    this.setState({ loading: false });
+
+    try {
+      this.setState({ loading: true, error: false });
+      const res = await fetch("https://fakestoreapi.com/products");
+      const data = await res.json();
+      const categories = data
+        .map((p) => p.category)
+        .filter((value, index, array) => array.indexOf(value) === index);
+      this.setState({ data, categories });
+      this.setState({ data, categories, loading: false });
+    } catch (e) {
+      this.setState({ error: true, loading: false });
+    }
+  };
+
   componentDidMount() {
-    fetch("https://fakestoreapi.com/products")
-      .then((res) => res.json())
-      .then((data) => {
-        // console.log(data);
-        this.setState({ data });
+    this.fetchProducts();
+    // fetch("https://fakestoreapi.com/products")
+    //   .then((res) => res.json())
+    //   .then((data) => {
+    //     // console.log(data);
+    //     this.setState({ data });
 
-        const categories = data
-          .map((p) => p.category)
-          .filter((value, index, array) => array.indexOf(value) === index);
+    //     const categories = data
+    //       .map((p) => p.category)
+    //       .filter((value, index, array) => array.indexOf(value) === index);
 
-        this.setState({ categories });
-      });
+    //     this.setState({ categories });
+    //   });
   }
 
   // categories = this.state.data
@@ -321,22 +345,46 @@ class App extends Component {
     //   .filter((value, index, array) => array.indexOf(value) === index);
     // this.setState({ category: categories[0] });
 
-    const { category, data, categories } = this.state;
+    const { category, data, categories, error, loading } = this.state;
 
     return (
       <div className="App">
-        <Header
-          categories={categories}
-          filterBySelected={this.filterBySelected}
-        />
+        {loading ? (
+          <Container maxWidth="sm">
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                marginTop: "10px",
+              }}
+            >
+              <CircularProgress
+                color="secondary"
+                sx={{ alignSelf: "center" }}
+              />
+            </Box>
+          </Container>
+        ) : error ? (
+          <>
+            <h1> There is an error </h1>
+            <button onClick={this.fetchProducts}>Try again</button>
+          </>
+        ) : (
+          <>
+            <Header
+              categories={categories}
+              filterBySelected={this.filterBySelected}
+            />
 
-        {/* <button onClick={() => this.setState({ show: !this.state.show })}>
-          {btnText}
-        </button> */}
+            {/* <button onClick={() => this.setState({ show: !this.state.show })}>
+            {btnText}
+          </button> */}
 
-        {/* {this.state.show && ( */}
-        <Products data={data} category={category} />
-        {/* )} */}
+            {/* {this.state.show && ( */}
+            <Products data={data} category={category} />
+            {/* )} */}
+          </>
+        )}
       </div>
     );
   }
